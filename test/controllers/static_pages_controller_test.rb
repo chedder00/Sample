ee198a7bb654533fc0f43defce1dfa7a248af24f
chedder00 <chedder00@gmail.com>
2014-11-29 22:56:08 +0000

    Refactored title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @title = "Sample"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title', :text => 'Home | Sample'
+    assert_select 'title', "#{@title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select 'title', :text => 'Help | Sample'
+    assert_select 'title', "Help | #{@title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title', :text => 'About | Sample'
+    assert_select 'title', "About | #{@title}"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select 'title', "Contact | #{@title}"
   end
   
 end
