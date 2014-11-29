82133c1b288b493339a42959439e9418d11f0591
chedder00 <chedder00@gmail.com>
2014-11-29 08:02:30 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', :text => 'Home | Sample'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', :text => 'Help | Sample'
   end
-
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select 'title', :text => 'About | Sample'
+  end
+  
 end
