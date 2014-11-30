9606da1521b02607a68dbaa8c542398bcfa70141
chedder00 <chedder00@gmail.com>
2014-11-30 01:13:11 +0000

    Finish layout and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+=begin
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if(@user.save)
+      redirect_to root_url
+    else
+      redirect_to signup_url
+    end
+=end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  #get 'static_pages/about'
+  #get 'static_pages/contact'
   
   root 'static_pages#home'
-
+  
+  get 'about' => 'static_pages#about', as: :about
+  get 'help' => 'static_pages#help', as: :help
+  get 'contact' => 'static_pages#contact', as: :contact
+  get 'signup' => 'users#new', as: :signup
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
